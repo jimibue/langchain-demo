f77d295aa49fba4bce5eccf1fe22ee4780dcac41
James Yeates <jamesyeates@jamess-mbp.lan>
2023-04-06 09:46:46 -0600

    yo

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("sd");
+import { OpenAI } from "langchain";
+
+const model = new OpenAI({
+  openAIApiKey: process.env.OPENAI_API_KEY,
+  temperature: 0.9,
+});
+
+const res = await model.call("What is a good name for a techno band?");
+
+console.log(res);
